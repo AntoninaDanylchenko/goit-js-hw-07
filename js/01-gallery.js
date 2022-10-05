62584e7d0927d801883d8061b1ae67bc26193c9b
TosiaDan <antonina.ivanets@gmail.com>
2022-10-05 19:31:10 +0200

    adds markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector(".gallery");
+const imageMarkup = createGalleryMarkup(galleryItems);
+imageContainer.insertAdjacentHTML("beforebegin", imageMarkup);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+                </div>`;
+    })
+    .join("");
+}
