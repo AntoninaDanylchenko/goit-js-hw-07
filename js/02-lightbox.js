92bc4bf9dbd45443b31891adc1945b7815d4482a
TosiaDan <antonina.ivanets@gmail.com>
2022-10-09 21:19:51 +0200

    corrects 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,9 +8,11 @@ imageContainer.insertAdjacentHTML("beforeend", imageMarkup);
 function createGalleryMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `      <a class="gallery__link" href="${original}">
+      return `    <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
                         <img class="gallery__image" src="${preview}" alt="${description}" />
                     </a>
+                  </li>
                `;
     })
     .join("");
